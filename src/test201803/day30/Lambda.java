ba81d9c5f30060ec0663e7f99200636e6d24b2a0
haoxiaolonga <haoxiaolonnga@gmail.com>
2018-04-03 18:12:11 +0800

    lambda了解基础知识

--- src/test201803/day30/Lambda.java
+++ src/test201803/day30/Lambda.java
@@ -62,12 +62,50 @@ public class Lambda {
     public void testPredicate(){
         List<String> langs = Arrays.asList("java","scala","net","phthon");
         System.out.println("以J开始的：");
-//        langs.forEach((str) -> str.startsWith("j"));
+        filter(langs,(str) -> str.toString().startsWith("j"));
+
+        System.out.println("以n结尾的：");
+        filter(langs,(str) -> str.toString().endsWith("n"));
+
+        System.out.println("输出所有：");
+        filter(langs,(str) -> true);
+
+        System.out.println("字符串长度大于4的：");
+        filter(langs,(str) -> str.toString().length() > 4);
+
+
     }
+    /**
+     * method_name: filter
+     * param: [list, condition]
+     * param: void
+     * describe: 用lambda和predicate做过滤器
+     * creat_user: haoxiaol
+     * creat_date: 2018/4/3
+     * creat_time: 9:38
+     **/
     public void filter(List<String> list, Predicate condition) {
+        /* 获取stream通过过滤条件 参数传入 condition 再遍历   */
         list.stream().filter((name) -> (condition.test(name))).forEach((name) -> {
             System.out.println(name + " ");
         });
     }
+    /**
+     * method_name: testMorePredicate
+     * param: []
+     * param: void
+     * describe: 测试多条件过滤
+     * creat_user: haoxiaol
+     * creat_date: 2018/4/3
+     * creat_time: 10:02
+     **/
+    @Test
+    public void testMorePredicate() {
+        List<String> langs = Arrays.asList("java","scala","net","phthon");
+        Predicate<String> conditionOne = (str) -> str.contains("a");
+        Predicate<String> conditionTwo = (str) -> str.length() == 4;
+        /*filter();*/
+    }
+
 
 }
