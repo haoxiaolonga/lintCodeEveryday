c1286b5440b12c710ae944230ba41d2e63f019fe
haoxiaolonga <haoxiaolonnga@gmail.com>
2018-08-15 10:35:33 +0800

    13章-练习17和18

--- src/tij/chapter13/Exercise17.java
+++ src/tij/chapter13/Exercise17.java
@@ -1,5 +1,11 @@
 package tij.chapter13;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * class_name: Exercise17
  * package: tij.chapter13
@@ -11,6 +17,50 @@ package tij.chapter13;
 public class Exercise17 {
 
     public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+
+//        String str = in.next();
+        String str = "G:\\GIT\\repository1\\lintCodeEveryday\\src\\tij\\chapter13\\Exercise10.java";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(str));
+            try {
+                //读入一行
+                String line = null;
+                //联系17
+                Pattern p = Pattern.compile("^[/**|*|//].*", Pattern.MULTILINE);
+                //练习18
+                Pattern p2 = Pattern.compile("[\"].*[\"]",Pattern.MULTILINE);
+                //练习19
+                Pattern p3 = Pattern.compile("",Pattern.MULTILINE);
+
+                Matcher m = p.matcher("");
+                Matcher m2 = p2.matcher("");
+                Matcher m3 = p3.matcher("");
+
+                while (null != (line = br.readLine())) {
+                    line = line.trim();
+                    m.reset(line);
+                    while (m.find()) {
+                        System.out.println(m.group());
+                    }
+
+                    m2.reset(line);
+                    while (m2.find()) {
+                        System.out.println("字符串" + m2.group());
+                    }
+                }
+
+            } catch (Exception e) {
+                System.out.println("读取文件出现异常");
+            } finally {
+                br.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("没有找到文件");
+
+        } catch (Exception e) {
+            System.out.println("创建文件流失败");
+        }
 
     }
 }
