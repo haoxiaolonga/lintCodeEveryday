8f2e5a4411c6cde64c63c789519599b35645269d
haoxiaolonga <haoxiaolonga@gmail.com>
2021-04-26 15:37:06 +0800

    add file bb.java

---
+++ src/BB.java
@@ -0,0 +1,13 @@
+/**
+ * class_name: BB
+ * package:  测试接口
+ * describe: TODO
+ * creat_user: haoxiaol
+ * creat_date: 2021/4/26
+ * creat_time: 下午3:34
+ **/
+public interface BB {
+
+    void spring(String string);
+
+}
